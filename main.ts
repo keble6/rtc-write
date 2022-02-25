424654600fafce6ce394ea9ad21c3c0c6c35a7ec
Rob Wilson <rob@rwilson.org.uk>
2022-02-25 11:52:02 +0000

    changed to 2 commands, st(ime) and sd(ate)

--- main.ts
+++ main.ts
@@ -1,64 +1,5 @@
 let stringIn = ""
 let command = ""
-let dataString = ""
-let dataNumber = 0
-function parseCommmand (command: string, number: number) {
-    if (command == "sY") {
-        DS3231.dateTime(
-        number,
-        DS3231.month(),
-        DS3231.date(),
-        DS3231.day(),
-        DS3231.hour(),
-        DS3231.minute(),
-        0
-        )
-    } else if (command == "sM") {
-        DS3231.dateTime(
-        DS3231.year(),
-        number,
-        DS3231.date(),
-        DS3231.day(),
-        DS3231.hour(),
-        DS3231.minute(),
-        0
-        )
-    } else if (command == "sD") {
-        DS3231.dateTime(
-        DS3231.year(),
-        DS3231.month(),
-        number,
-        DS3231.day(),
-        DS3231.hour(),
-        DS3231.minute(),
-        0
-        )
-    } else if (command == "sh") {
-        DS3231.dateTime(
-        DS3231.year(),
-        DS3231.month(),
-        DS3231.date(),
-        DS3231.day(),
-        number,
-        DS3231.minute(),
-        0
-        )
-    } else if (command == "sm") {
-        DS3231.dateTime(
-        DS3231.year(),
-        DS3231.month(),
-        DS3231.date(),
-        DS3231.day(),
-        0,
-        number,
-        0
-        )
-    } else if (command == "dt") {
-        serial.writeLine("" + leadingZero(DS3231.date()) + "/" + leadingZero(DS3231.month()) + "/" + DS3231.year() + "  " + leadingZero(DS3231.hour()) + ":" + leadingZero(DS3231.minute()) + ":" + leadingZero(DS3231.second()))
-    } else {
-        serial.writeLine("Invalid command")
-    }
-}
 function leadingZero (num: number) {
     if (num < 10) {
         return "0" + num
@@ -70,8 +11,32 @@ basic.forever(function () {
     stringIn = serial.readUntil(serial.delimiters(Delimiters.NewLine))
     if (stringIn.length >= 2) {
         command = stringIn.substr(0, 2)
-        dataString = stringIn.substr(2, stringIn.length - 2)
-        dataNumber = parseFloat(dataString)
-        parseCommmand(command, dataNumber)
+        if (command == "st") {
+            DS3231.dateTime(
+            DS3231.year(),
+            DS3231.month(),
+            DS3231.date(),
+            DS3231.day(),
+            parseFloat(stringIn.substr(2, 2)),
+            parseFloat(stringIn.substr(4, 2)),
+            0
+            )
+            basic.showNumber(DS3231.hour())
+            basic.showNumber(DS3231.minute())
+        }
+        if (command == "sd") {
+            DS3231.dateTime(
+            parseFloat(stringIn.substr(2, 2)),
+            parseFloat(stringIn.substr(4, 2)),
+            parseFloat(stringIn.substr(6, 2)),
+            DS3231.day(),
+            DS3231.hour(),
+            DS3231.minute(),
+            0
+            )
+            basic.showNumber(DS3231.date())
+            basic.showNumber(DS3231.month())
+            basic.showNumber(DS3231.year())
+        }
     }
 })
