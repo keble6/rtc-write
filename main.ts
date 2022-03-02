5b18e883f98535065425896b792e75ae7557a97a
Rob Wilson <rob@rwilson.org.uk>
2022-03-02 10:12:36 +0000

    separate functions

--- main.ts
+++ main.ts
@@ -1,10 +1,21 @@
+let date = ""
 let stringIn = ""
+let month = ""
+let year = ""
+let minute = ""
+let hour = ""
 let command = ""
 function setDate () {
+    // the first 2 characters after command
+    date = stringIn.substr(2, 2)
+    // the next 2 characters
+    month = stringIn.substr(4, 2)
+    // the last 4 characters
+    year = stringIn.substr(6, 4)
     DS3231.dateTime(
-    parseFloat(stringIn.substr(6, 4)),
-    parseFloat(stringIn.substr(4, 2)),
-    parseFloat(stringIn.substr(2, 2)),
+    parseFloat(year),
+    parseFloat(month),
+    parseFloat(date),
     DS3231.day(),
     DS3231.hour(),
     DS3231.minute(),
@@ -15,12 +26,16 @@ function setDate () {
     basic.showNumber(DS3231.year())
 }
 function setTime () {
+    // the first 2 characters after command
+    minute = stringIn.substr(2, 2)
+    // the next 2 characters command
+    hour = stringIn.substr(4, 2)
     DS3231.dateTime(
     DS3231.year(),
     DS3231.month(),
     DS3231.date(),
     DS3231.day(),
-    parseFloat(stringIn.substr(2, 2)),
+    parseFloat(date),
     parseFloat(stringIn.substr(4, 2)),
     0
     )
