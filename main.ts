20ed8bad34446f2de8ec621b408238fab732ed4a
Rob Wilson <rob@rwilson.org.uk>
2022-03-02 09:36:58 +0000

    separate functions

--- main.ts
+++ main.ts
@@ -1,35 +1,39 @@
 let stringIn = ""
 let command = ""
+function setDate () {
+    DS3231.dateTime(
+    parseFloat(stringIn.substr(6, 4)),
+    parseFloat(stringIn.substr(4, 2)),
+    parseFloat(stringIn.substr(2, 2)),
+    DS3231.day(),
+    DS3231.hour(),
+    DS3231.minute(),
+    0
+    )
+    basic.showNumber(DS3231.date())
+    basic.showNumber(DS3231.month())
+    basic.showNumber(DS3231.year())
+}
+function setTime () {
+    DS3231.dateTime(
+    DS3231.year(),
+    DS3231.month(),
+    DS3231.date(),
+    DS3231.day(),
+    parseFloat(stringIn.substr(2, 2)),
+    parseFloat(stringIn.substr(4, 2)),
+    0
+    )
+    basic.showNumber(DS3231.hour())
+    basic.showNumber(DS3231.minute())
+}
 serial.onDataReceived(serial.delimiters(Delimiters.CarriageReturn), function () {
     stringIn = serial.readUntil(serial.delimiters(Delimiters.CarriageReturn))
-    if (stringIn.length >= 2) {
-        command = stringIn.substr(0, 2)
-        if (command == "st") {
-            DS3231.dateTime(
-            DS3231.year(),
-            DS3231.month(),
-            DS3231.date(),
-            DS3231.day(),
-            parseFloat(stringIn.substr(2, 2)),
-            parseFloat(stringIn.substr(4, 2)),
-            0
-            )
-            basic.showNumber(DS3231.hour())
-            basic.showNumber(DS3231.minute())
-        }
-        if (command == "sd") {
-            DS3231.dateTime(
-            parseFloat(stringIn.substr(6, 4)),
-            parseFloat(stringIn.substr(4, 2)),
-            parseFloat(stringIn.substr(2, 2)),
-            DS3231.day(),
-            DS3231.hour(),
-            DS3231.minute(),
-            0
-            )
-            basic.showNumber(DS3231.date())
-            basic.showNumber(DS3231.month())
-            basic.showNumber(DS3231.year())
-        }
+    command = stringIn.substr(0, 2)
+    if (command == "st") {
+        setTime()
+    }
+    if (command == "sd") {
+        setDate()
     }
 })
